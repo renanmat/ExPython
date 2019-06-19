c400b458b50c339f5960fac191d9264be0d7d7b9
Silver <renanmatheus09@gmail.com>
2019-06-19 12:15:32 -0300

    renomeado arq de 65 a 99

--- ex72.py
+++
@@ -1,9 +0,0 @@
-numero = (
-    'zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
-    'quatorze', 'quinze', 'dezeseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
-while True:
-    n = ''
-    n = int(input('Digite um numero: '))
-    if n < 0:
-        break
-    print(f'voce digitou o numero {numero[n]}')
--- ex96.py
+++
@@ -1,9 +0,0 @@
-def area(largura, comprimento):
-    are = largura * comprimento
-    print(f'A are do terreno {largura} X {comprimento} é igual {are}m2.')
-
-
-# Programa principal
-c = float(input('Qual a largura do terreno? '))
-l = float(input('Qual o comprimento? '))
-area(c, l)
